8ef4b3783d4aef1109b2fddd67076f43d579e847
Kostya Podlesniy <konstantin-921@yandex.ru>
2018-08-04 23:16:10 +0300

    Fix verifytoken exclude

--- services/verifytoken.js
+++ services/verifytoken.js
@@ -3,7 +3,8 @@ const config = require('../config/config');
 
 module.exports = (req, res, next) => {
   let auth = req.get('Authorization');
-  if (auth && req.url !== '/auth/registration') {
+  let path = req.url.substring(0, 11);
+  if (auth && req.url !== '/auth/registration' && path !== '/auth/login') {
     let token = auth.substring(7);
     jwt.verify(token, config.secretOrKey, { ignoreExpiration: false }, function (err, decoded) {
       if (err) {
